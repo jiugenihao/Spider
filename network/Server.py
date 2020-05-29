d8fd3bc3c81d2c4e4e331117d7c7762609278462
hanzhongping <360420509@qq.com>
2020-05-29 18:43:53 +0800

    network test

--- network/Server.py
+++ network/Server.py
@@ -1,16 +1,24 @@
 import socket
 
-server_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_sock.bind(('localhost', 6999))
-server_sock.listen(5)
+LISTEN_NUM = 5
+BUFSIZE = 1024
+ip_port = ('localhost', 6999)
 
-#while True:
-    conn, addr = server_sock.accept()
-    print(conn, addr)
+server_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+host = socket.gethostname()
+port = 6999
+server_sock.bind((host, port))
+#server_sock.bind(ip_port)
+server_sock.listen(LISTEN_NUM)
+
+while True:
+    conn, cli_addr = server_sock.accept()
+    print("连接地址：%s" % str(cli_addr))
+    #print(conn, cli_addr)
 
     #while True:
     try:
-        data = conn.recv(1024)
+        data = conn.recv(BUFSIZE)
         print("receive:", data.decode())
         conn.send(data.upper())
     except ConnectionResetError  as e:
